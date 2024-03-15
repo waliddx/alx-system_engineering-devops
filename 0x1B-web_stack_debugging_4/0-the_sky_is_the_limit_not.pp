fb374e489f1bece7ca7c7340542591e94243f3ca
waliddx <wagzouly@gmail.com>
2024-03-15 00:31:26 +0000

    x

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
-nx to accept and serve more requests
+#fix nginx to accept and serve more requests
 
 exec {'modify max open files limit setting':
   command => 'sed -i "s/15/10000/" /etc/default/nginx && sudo service nginx restart',
